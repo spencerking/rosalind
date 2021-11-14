fff81148bbb41b19f7681f91bb16164822f9de80
Spencer King <spencer@spencertking.com>
2021-11-14 14:51:48 -0600

    ini3 - strings and lists

---
+++ src/python-village/ini3-strings-and-lists.py
@@ -0,0 +1,4 @@
+def main(s, a, b, c, d):
+    return s[a:b+1] + " " + s[c:d+1]
+
+print(main("qZk6iWJE8y76AYPmzYiS3Y4t53uRrMCtenosauraoBwuGP5NOR9NIiG9juKqU1kOuUxBlg8FlZHthbrachydactyla0hpF7OLyEuPjOghpVjTkwxL3FaAhEoWSJUoPvZ5kQjRbvsEpMDapHWbbmO6cY8ZXjl8hzjbWxaUhvwZ9LcPJr5LDL0DMcRmTkeGntP1QF53.", 30, 39, 77, 89))
